b5e816f0be176f5ad8df920862427ad01ce4f41d
[KAO] Udomsub Naikorn <udomsub@mintel.tech>
2020-04-04 17:04:09 +0700

    :construction: [Controller] Use @ResController instead of @Controller

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -3,22 +3,17 @@ package com.example.springboot.controller;
 import com.example.springboot.domain.Employee;
 import com.example.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Controller
-// @ResponseBody is field mapper
+@RestController
 @RequestMapping("/employees")
-@ResponseBody
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    // API
     @GetMapping
     public List<Employee> findAll() {
         return employeeService.listAllEmployees();
@@ -30,7 +25,8 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public void create(@RequestBody Employee employee) {
+    public Employee create(@RequestBody Employee employee) {
         employeeService.save(employee);
+        return employee;
     }
 }
