071c995deea2cd2252187c7eebce2d847c457694
[KAO] Udomsub Naikorn <udomsub@mintel.tech>
2020-04-04 16:04:02 +0700

    :art: [API] get PathVariable id from GET params

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.springboot.controller;
 
 import com.example.springboot.domain.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,8 +12,8 @@ import java.util.List;
 @Controller
 public class EmployeeController {
 
+    // API
     @RequestMapping("/employees")
-
     // @ResponseBody is field mapper
     @ResponseBody
     public List<Employee> findAll() {
@@ -20,6 +21,13 @@ public class EmployeeController {
         employeeList.add(new Employee("John", "Doe"));
         employeeList.add(new Employee("Kao", "Jan"));
         return employeeList;
+    }
+
+    @RequestMapping("/employees/{id}")
+    @ResponseBody
+    public Employee getEmployee(@PathVariable Integer id) {
+        System.out.println("id: " + id);
+        return new Employee("John", "Doe");
     }
 
 }
